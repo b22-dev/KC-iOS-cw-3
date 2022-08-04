61301398c4e157fdd2b82958be49f7143e430bbb
b22-dev <99684336+b22-dev@users.noreply.github.com>
2022-08-04 14:58:34 +0300

    playgroundcw3

---
+++ playgroundcw3.playground/Contents.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+var name: String = "Ahmed"
+var hobby: String = "Tennis"
+var favouriteNumber: Int = 23
+let BirthOfDate: Int = 2006
+
+print("Hi, my name is \(name).\t and my favourite number is \(favouriteNumber) and i like \(hobby).\nAnd i was born in \(BirthOfDate)")
